661941db23432fff63507d231cd0094f21cc7bf0
it <it000946@gmail.com>
2022-01-21 22:30:39 +0200

    removed commented out code from previous commit

--- socd.c
+++ socd.c
@@ -67,7 +67,6 @@ const char *BY_ID = "/dev/input/by-id/";
 // Some devices may not have the above so this might work instead
 const char *BY_PATH = "/dev/input/by-path/";
 
-// int get_keyboard(void);
 int get_keyboard(const char *path);
 int prompt_user(int max);
 
@@ -102,11 +101,6 @@ int main(int argc, char **argv) {
         fprintf(stderr, "error: Failed to get keyboards\n");
         exit(1);   
     }
-
-    // if (get_keyboard()) {
-    //     fprintf(stderr, "error: Failed to get keyboards\n");
-    //     exit(1);
-    // }
 
     setup_write();
     
@@ -321,13 +315,6 @@ void emit_all() {
 int get_keyboard(const char *path) {
     DIR *d;
     struct dirent *dir;
-
-    // if ((d = opendir(BY_ID)) != NULL)
-    //     strcpy(context.rd_target, BY_ID);
-    // else if ((d = opendir(BY_PATH)) != NULL)
-    //     strcpy(context.rd_target, BY_PATH);
-    // else 
-    //     return 1;
 
     if ((d = opendir(path)) != NULL)
         strcpy(context.rd_target, path);
